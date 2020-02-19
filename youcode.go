9a5db1c1fd679e1f0051692b4aa427c406575448
aungbar001 <aungieb0977@gmail.com>
2020-02-19 22:08:15 +0000

    Initial commit

---
+++ youcode.go
@@ -0,0 +1,48 @@
+//Name:Aungelia Barletta
+//Date:2/18/20
+//Description:
+
+package main
+
+import(
+  "fmt"
+  "math/rand")
+
+
+func main() {
+  var count int
+  
+  min := 0
+  max > 0
+
+ fmt.Println(rand.Intn(max - min + 1) + min) 
+  
+  fmt.Print("What is the highist number it could be?")
+  var max int
+  fmt.Scanb("%b", &max)
+  fmt.Printb("The max is:")
+
+  var computerGuess = rand.Intn (max)
+
+  fmt.Print("Guess the computer number")
+  var userGuess int
+  fmt.Sacnb("%b", &userGuess)
+  fmt.Printb(userGuess)
+
+  for userGuess := rand.Intn; computerGuess == userGuess; userGuess++ {
+    fmt.Println("your guess:", &userGuess)
+   
+
+
+  }
+
+
+
+
+
+
+
+
+
+  
+}
